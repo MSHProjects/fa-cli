be1c5bfc194fa641d3deb2230f6425110216b88e
SolaimenHammami <soulaimen@iovision>
2024-06-10 00:41:10 +0100

    update readocs.yml

--- source/conf.py
+++ source/conf.py
@@ -7,6 +7,16 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+# Define the output directory
+# Default to '_build' if READTHEDOCS_OUTPUT is not set
+output_dir = os.environ.get('READTHEDOCS_OUTPUT', '_build')
+html_output_dir = os.path.join(output_dir, 'html')
+
+# Update the configuration to use the html_output_dir
+html_context = {
+    'build_dir': html_output_dir,
+}
+
 sys.path.insert(0, os.path.abspath('..'))
 project = 'fa-cli'
 copyright = '2024, Soulaimen Hammami'
